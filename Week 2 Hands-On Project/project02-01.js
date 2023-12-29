5db531784abd9cbe5c0c127e8c68e088756fa16e
Erickkthemage <155121981+Erickkthemage@users.noreply.github.com>
2023-12-29 01:31:13 -0500

    All projects for this class

---
+++ Week 2 Hands-On Project/project02-01.js
@@ -0,0 +1,18 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-01
+
+      Celsius <-> Farenheit Coverter
+      Author: Erick Diaz
+      Date:   3/13/23
+
+      Filename: project02-01.js
+ */
+
+function FahrenheitToCelsius(degree){
+      return (degree - 32) / 1.8;
+}
+
+function CelsiusToFahrenheit(degree){
+      return degree * 1.8 + 32;
+}
